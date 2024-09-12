066cc4f7a222646519d7b64474194c9900cbef55
Susi <braeden8.susi6@gmail.com>
2024-09-12 16:57:55 -0400

    Controller in place

--- Assets/Runtime/WokController.cs
+++ Assets/Runtime/WokController.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 using static UnityEditor.Timeline.TimelinePlaybackControls;
+using static UnityEngine.Rendering.DebugUI;
 
 public class WokController : MonoBehaviour
 {
     public InputActionAsset actions;
     Vector2 tilt = Vector2.zero;
+
+    [Header("Movement Parameters")]
+    private float angleForward = 30f;
+    private float angleBackward = -30f;
+    private float angleLeft = -30f;
+    private float angleRight = 30f;
+    private float deltaForward = .5f;
+    private float deltaBackward = -2f;
     // Start is called before the first frame update
     protected void Awake()
     {
@@ -19,9 +29,14 @@ public class WokController : MonoBehaviour
         tilt = actions["TiltKeys"].ReadValue<Vector2>();
         tilt.x *= -1;
         Vector3 tempTilt = Vector3.zero;
-        tempTilt.x = (tilt.y - (-1)) / (1 - (-1)) * (30 - (-30)) + (-30);
-        tempTilt.z = (tilt.x - (-1)) / (1 - (-1)) * (30 - (-30)) + (-30);
+        tempTilt.x = (tilt.y + 1) / (1 + 1) * (angleForward - angleBackward) + (angleBackward);
+        tempTilt.z = (tilt.x + 1) / (1 + 1) * (angleForward - angleBackward) + (angleBackward);
         transform.rotation = Quaternion.Euler(tempTilt);
+       
+        Vector3 tempLoc = Vector3.zero;
+        tempLoc.z = (tilt.y + 1) / (1 +1 ) * (deltaForward - deltaBackward) + (deltaBackward);
+        transform.position = tempLoc;
+
         Debug.Log(tempTilt.x + " " + tempTilt.z);
     }
 
